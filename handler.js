57224a9ecdf0cef1a18df78167cd37960faabe80
David Gray <david.gray2@griffithuni.edu.au>
2020-08-19 02:56:38 +1000

    Finished handler

--- handler.js
+++ handler.js
@@ -1,18 +1,24 @@
 'use strict';
 
-module.exports.rankme = async event => {
-  return {
+const emojis = [
+  '😄','😃','😀','😊','😉','😍','🔶','🔷', '🚀'
+];
+module.exports.rankme = (event, context, callback) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Rank generated!',
+      input: rankEmoji,
+    }),
   };
 
+  callback(null, response);
+
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
